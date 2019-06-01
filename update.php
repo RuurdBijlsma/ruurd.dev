4cc5e2926dfa246391b62a8e76ab36c4de8262d2
Ruurd Bijlsma <ruurd@bijlsma.dev>
2019-06-01 23:56:08 +0000

    update

--- update.php
+++ update.php
@@ -1,8 +1,9 @@
 <?php
 
 // Use in the “Post-Receive URLs” section of your GitHub repo.
-
+echo "doei";
 if ( $_POST['payload'] ) {
+echo "hoi";
 shell_exec( 'cd /home/ruurd/web/ruurd.dev/ && git reset –hard HEAD && git pull' );
 }
 
