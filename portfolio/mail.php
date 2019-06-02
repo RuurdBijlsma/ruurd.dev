2b8b5fcd9becca07ed42c5b2da8b02fdce9a660c
Ruurd Bijlsma <ruurd@bijlsma.dev>
2019-06-02 22:08:04 +0200

    update

--- portfolio/mail.php
+++ portfolio/mail.php
@@ -15,7 +15,7 @@
             $headers = "MIME-Version: 1.0" . "\r\n";
             $headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
             $headers .= "X-MSMail-Priority: High" . "\r\n";
-            $headers .= "From: " . $from . "\r\n";
+            $headers .= "From: ruurd@bijlsma.dev\r\n";
             $headers .= "Reply-To: " . $from . "\r\n";
             $headers .= 'X-Mailer: PHP/' . phpversion();
 
