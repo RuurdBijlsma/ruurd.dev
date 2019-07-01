334c850ba89a8d9cbc81db66278375d22e34592c
Ruurd Bijlsma <ruurd@bijlsma.dev>
2019-07-01 15:35:13 +0200

    update

---
+++ pesten/precache-manifest.00eaceb1b822897218fd26982568c3f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf753240123a835e4b86",
+    "url": "/pesten/css/app.99e4cb45.css"
+  },
+  {
+    "revision": "cf753240123a835e4b86",
+    "url": "/pesten/js/app.2a8d9413.js"
+  },
+  {
+    "revision": "378d3487fe92dcf07896",
+    "url": "/pesten/css/chunk-vendors.45b44fe4.css"
+  },
+  {
+    "revision": "378d3487fe92dcf07896",
+    "url": "/pesten/js/chunk-vendors.43400b6a.js"
+  },
+  {
+    "revision": "6ad3b8f45e5eb066c07c693c109dad44",
+    "url": "/pesten/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pesten/robots.txt"
+  }
+];
