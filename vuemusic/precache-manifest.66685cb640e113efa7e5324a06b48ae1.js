6fbc6033bba66d130ad84d01e15e2e0d98efea22
Ruurd Bijlsma <ruurd@bijlsma.dev>
2019-09-17 23:51:44 +0200

    update

---
+++ vuemusic/precache-manifest.66685cb640e113efa7e5324a06b48ae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "204d7fd464765f65e899",
+    "url": "/vuemusic/css/app.37d46bf9.css"
+  },
+  {
+    "revision": "204d7fd464765f65e899",
+    "url": "/vuemusic/js/app.8c9d1732.js"
+  },
+  {
+    "revision": "8d8073e3f614d6286e67",
+    "url": "/vuemusic/css/chunk-vendors.33fbaed9.css"
+  },
+  {
+    "revision": "8d8073e3f614d6286e67",
+    "url": "/vuemusic/js/chunk-vendors.3df282af.js"
+  },
+  {
+    "revision": "0c56fe9e2a27e9f8931e8644bd81bd5c",
+    "url": "/vuemusic/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vuemusic/robots.txt"
+  }
+];
--- vuemusic/service-worker.js
+++ vuemusic/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vuemusic/precache-manifest.d2c4372273da2022b58332b495517c4f.js"
+  "/vuemusic/precache-manifest.66685cb640e113efa7e5324a06b48ae1.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vuemusic-3"});
