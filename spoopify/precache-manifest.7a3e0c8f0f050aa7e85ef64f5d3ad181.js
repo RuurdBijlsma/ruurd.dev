3cc892787bdabffefa2826643c73330c0e6abeaa
Ruurd Bijlsma <ruurd@bijlsma.dev>
2019-12-12 04:37:34 +0100

    update

---
+++ spoopify/precache-manifest.7a3e0c8f0f050aa7e85ef64f5d3ad181.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d93b5ebe1bbbf4cd32",
+    "url": "spoopify/css/about.e6e3f148.css"
+  },
+  {
+    "revision": "aa623d7481059237f3c5",
+    "url": "spoopify/css/app.21337ba2.css"
+  },
+  {
+    "revision": "841a15cde12f8c9098e1",
+    "url": "spoopify/css/chunk-vendors.896a25bc.css"
+  },
+  {
+    "revision": "622849649192b0772581b39dc56bb64f",
+    "url": "spoopify/img/notfound.png"
+  },
+  {
+    "revision": "a5380cffde67ff96c71d26a67f2cccaf",
+    "url": "spoopify/index.html"
+  },
+  {
+    "revision": "f3d93b5ebe1bbbf4cd32",
+    "url": "spoopify/js/about.f6641cad.js"
+  },
+  {
+    "revision": "aa623d7481059237f3c5",
+    "url": "spoopify/js/app.a5580044.js"
+  },
+  {
+    "revision": "841a15cde12f8c9098e1",
+    "url": "spoopify/js/chunk-vendors.5e069b27.js"
+  },
+  {
+    "revision": "ec63a3eea4e7f6109fab4312faa3c46c",
+    "url": "spoopify/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "spoopify/robots.txt"
+  }
+]);
