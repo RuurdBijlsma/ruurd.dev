dd3a63fb8a80cf0d3180f7c7a0a9219af6da1b47
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-06-17 02:48:48 +0200

    update

---
+++ minesweeper/precache-manifest.0a72ebe47a1c5dc9f959ca7c6ba6e6c2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6994912978662f37450",
+    "url": "/minesweeper/css/app.b51a4460.css"
+  },
+  {
+    "revision": "fd7384e2e4b941c20ee9",
+    "url": "/minesweeper/css/chunk-vendors.9a782cc8.css"
+  },
+  {
+    "revision": "421944b3a364461040ab112eef787545",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "a6994912978662f37450",
+    "url": "/minesweeper/js/app.8d655a45.js"
+  },
+  {
+    "revision": "3ae0b826089c84cc6735",
+    "url": "/minesweeper/js/chunk-2d22d746.06bdeebe.js"
+  },
+  {
+    "revision": "fd7384e2e4b941c20ee9",
+    "url": "/minesweeper/js/chunk-vendors.ab0b59cc.js"
+  },
+  {
+    "revision": "4dd2751dfa44c648270334e74aa5aa10",
+    "url": "/minesweeper/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/minesweeper/robots.txt"
+  }
+]);
