55daeb290681dc5c1cefd0b38323122592bb5585
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-07-01 01:07:30 +0200

    update

---
+++ mind/precache-manifest.53fee5c7f50444f06b4a03d3a1a9cdee.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baee971d0ea6d95226d4",
+    "url": "/mind/css/app.d97dca6f.css"
+  },
+  {
+    "revision": "321f475e4e8bbe366f73",
+    "url": "/mind/css/chunk-72f62960.cd0ea64a.css"
+  },
+  {
+    "revision": "73df2335e996bf4e5b58",
+    "url": "/mind/css/chunk-vendors.8c17bd69.css"
+  },
+  {
+    "revision": "fb63d4531328218c160cfc80f796717c",
+    "url": "/mind/img/card-back.fb63d453.png"
+  },
+  {
+    "revision": "8d03df8f712482c63bb941b232cc9658",
+    "url": "/mind/img/card-background.8d03df8f.png"
+  },
+  {
+    "revision": "a74efa020699f71023e8b8b9176fd0e1",
+    "url": "/mind/index.html"
+  },
+  {
+    "revision": "baee971d0ea6d95226d4",
+    "url": "/mind/js/app.11a012d9.js"
+  },
+  {
+    "revision": "321f475e4e8bbe366f73",
+    "url": "/mind/js/chunk-72f62960.517a0da9.js"
+  },
+  {
+    "revision": "73df2335e996bf4e5b58",
+    "url": "/mind/js/chunk-vendors.25b52ab1.js"
+  },
+  {
+    "revision": "bcd90bc9c10b5e068715005d4a4ad3a7",
+    "url": "/mind/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/mind/robots.txt"
+  }
+]);
