727dfb077e905053832a9328cdaa428db34bd364
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-08-22 01:34:22 +0200

    update

--- deleteme/script.js
+++ deleteme/script.js
@@ -3,15 +3,16 @@ document.addEventListener('DOMContentLoaded', init, false);
 async function init() {
     console.log("WEBSITE UP AND RUNNING!")
 
+    ele = document.querySelector('.content');
     try{
         response = await fetch('https://example.com', {mode: 'cors'})
         text = await response.text();
         console.log(text)
-        document.write(text);
+        ele.innerText(text);
     }
     catch(e){
         console.error(e);
-        document.write(e.message);
+        ele.innerText(e.message);
     }
 
 }
