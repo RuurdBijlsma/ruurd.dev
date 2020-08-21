9cd7f12311116cf512b5d8a531ff8855e76b599d
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-08-22 01:22:01 +0200

    update

--- deleteme/script.js
+++ deleteme/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', init, false);
 
 function init() {
+    console.log("WEBSITE UP AND RUNNING!")
+
     response = await fetch('https://example.com')
     console.log(await response.text())
 }
