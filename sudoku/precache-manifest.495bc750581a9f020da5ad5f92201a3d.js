9a05b4f4a2ccddb3da3db17e87d9f73888f5e0f1
Ruurd Bijlsma <ruurd@bijlsma.dev>
2020-10-15 04:15:34 +0200

    update

---
+++ sudoku/precache-manifest.495bc750581a9f020da5ad5f92201a3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "389322c557d87ed316f3",
+    "url": "/sudoku/css/app.a9be9b82.css"
+  },
+  {
+    "revision": "e7ed8346d8c98303aecd",
+    "url": "/sudoku/css/chunk-vendors.72918a47.css"
+  },
+  {
+    "revision": "617f03c9405446f33f88a9580b17bf40",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "389322c557d87ed316f3",
+    "url": "/sudoku/js/app.f97d387f.js"
+  },
+  {
+    "revision": "f1b8662c90d28d13ed18",
+    "url": "/sudoku/js/chunk-2d22d746.5a6734f2.js"
+  },
+  {
+    "revision": "e7ed8346d8c98303aecd",
+    "url": "/sudoku/js/chunk-vendors.8f4052bd.js"
+  },
+  {
+    "revision": "c0f8a50342557a4914756f7a8fa2995e",
+    "url": "/sudoku/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sudoku/robots.txt"
+  }
+]);
