d476b50aa2c39f1c8e2713cd0cb5e03917ce18fb
Ruurd Bijlsma <ruurd@bijlsma.dev>
2021-05-21 17:53:35 +0200

    update

---
+++ photos/precache-manifest.5b6425b4d9518a33ee7bab3ac9f399db.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00bd7c6e9d61766c1ad5",
+    "url": "/photos/css/app.2ef39413.css"
+  },
+  {
+    "revision": "892daaffec575f1044e7",
+    "url": "/photos/css/chunk-704590ec.2efca91c.css"
+  },
+  {
+    "revision": "722340dce933ad9e449f",
+    "url": "/photos/css/chunk-a519ce8c.0e539c7e.css"
+  },
+  {
+    "revision": "2148e202e251f9ce750e",
+    "url": "/photos/css/chunk-vendors.df3600ba.css"
+  },
+  {
+    "revision": "5f11d4ca8c26e34cde3d78206036e57f",
+    "url": "/photos/img/icon.png"
+  },
+  {
+    "revision": "e6e999bf84534ffdef06f712675faf01",
+    "url": "/photos/img/transparent-icon-512.e6e999bf.png"
+  },
+  {
+    "revision": "9f89c36fa5cffa430eb86fe1328022ea",
+    "url": "/photos/index.html"
+  },
+  {
+    "revision": "00bd7c6e9d61766c1ad5",
+    "url": "/photos/js/app.a35294b7.js"
+  },
+  {
+    "revision": "892daaffec575f1044e7",
+    "url": "/photos/js/chunk-704590ec.8276b1d6.js"
+  },
+  {
+    "revision": "722340dce933ad9e449f",
+    "url": "/photos/js/chunk-a519ce8c.a2ce60b3.js"
+  },
+  {
+    "revision": "2148e202e251f9ce750e",
+    "url": "/photos/js/chunk-vendors.99846976.js"
+  },
+  {
+    "revision": "74da3f4297d16d6f3f7b99aa38bb399a",
+    "url": "/photos/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/photos/robots.txt"
+  }
+]);
