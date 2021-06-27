ed1918132ff4631c4559d9441441cf8737df0a38
Ruurd Bijlsma <ruurd@bijlsma.dev>
2021-06-27 23:34:46 +0200

    update

--- alphacolorpicker/script.js
+++ alphacolorpicker/script.js
@@ -1,11 +1,12 @@
 document.addEventListener('DOMContentLoaded', init, false);
+scale = 1;
 
 function init() {
     canvas = document.querySelector('canvas');
     context = canvas.getContext('2d');
     canvas.width = 256;
     canvas.height = 256;
-    mouseDown=false;
+    mouseDown = false;
 
     canvas.addEventListener('mousedown', e => {
         mouseDown = true;
@@ -19,12 +20,12 @@ function init() {
     });
 }
 function doIt(e) {
-    if(mouseDown){
+    if (mouseDown) {
         let x = e.pageX - canvas.getBoundingClientRect().left;
         let y = e.pageY - canvas.getBoundingClientRect().top;
-        let imageData = context.getImageData(x, y, 1, 1);
+        let imageData = context.getImageData(x / scale, y / scale, 1, 1);
         let [r, g, b, a] = imageData.data;
-        document.querySelector('.output').innerText = `rgba(${r},${g},${b},${Math.floor(a / 255 * 100) / 100})`
+        document.querySelector('.output').innerText = `rgba(${r}, ${g}, ${b}, ${Math.floor(a / 255 * 100) / 100})`
     }
 }
 function draw(image) {
@@ -33,10 +34,13 @@ function draw(image) {
     img.src = URL.createObjectURL(image);
     img.onload = () => {
         console.log(img, img.width, img.height);
+        scale = 1;
+        if (img.width < window.innerWidth / 2 && img.height < window.innerHeight)
+            scale = Math.floor(window.innerWidth / img.width);
         canvas.width = img.width;
         canvas.height = img.height;
-        canvas.style.width = img.width + 'px';
-        canvas.style.height = img.height + 'px';
+        canvas.style.width = img.width * scale + 'px';
+        canvas.style.height =  img.height * scale + 'px';
         context.drawImage(img, 0, 0);
     }
 }
